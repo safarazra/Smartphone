951246f47dc242fcf940c371318699f0e09fe6c0
safarazra <raraz.270310@gmail.com>
2026-04-17 10:26:26 +0700

    Abstraction Ismartphone

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ISmartphone hpKapibara = new IPhone();
+
+Fotografer fotografer =  new Fotografer();
+fotografer.Kerja(hpKapibara);
+
+hpKapibara = new Samsung();
+fotografer.Kerja(hpKapibara);
+
+interface ISmartphone
+{
+    public void AmbilFoto();
+}
+
+class IPhone : ISmartphone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan sensor kamera premium... *cekrek!*");
+    }
+}
+
+class Samsung : ISmartphone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan fitur beauty... *cekrek!*");
+    }
+}
+
+class Fotografer
+{
+    public void Kerja(ISmartphone hp)
+    {
+        hp.AmbilFoto();
+    }
+}
